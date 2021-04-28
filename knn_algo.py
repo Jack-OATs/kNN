9e27d4e50ea458b7da1f2ba83d80bb37b0cf910d
Emmanuel <eia1803@hsutx.edu>
2021-04-28 17:55:11 -0500

    editing load_csv()

--- knn_algo.py
+++ knn_algo.py
@@ -1,5 +1,5 @@
 import math
-
+import numpy as np
 
 def euclidean(all_x, all_y):
     all_points = zip(all_x, all_y)
@@ -7,3 +7,48 @@ def euclidean(all_x, all_y):
     for x, y in all_points:
         set_diff.append(math.pow(x-y, 2))
     return math.sqrt(math.fsum(set_diff))
+
+
+def nearest_neighbor(train_data, test_data, num_of_neighbor):
+        dist_list = []
+        for row in train_data:
+            dist = euclidean(row, test_data)
+            dist_list.append((dist, test_data))
+        dist_list.sort(key=lambda d: d[0])
+        neighbor = []
+        for i in range(num_of_neighbor):
+            neighbor.append(dist_list[i][1])
+        return neighbor
+
+
+def classify(train_data, test_data, num_of_neighbor):
+    neighbors = nearest_neighbor(train_data, test_data, num_of_neighbor)
+    class_vals = [neighbor[-1] for neighbor in neighbors]
+    pred = [lambda vals: max(vals, key=vals.count),
+            lambda vals: np.average(vals)]
+    preds = []
+    for p in pred:
+        preds.append(p(class_vals))
+    return preds
+
+
+def get_example():
+    return [[2.7810836,2.550537003,0],
+        [1.465489372,2.362125076,0],
+        [3.396561688,4.400293529,0],
+        [1.38807019,1.850220317,0],
+        [3.06407232,3.005305973,0],
+        [7.627531214,2.759262235,1],
+        [5.332441248,2.088626775,1],
+        [6.922596716,1.77106367,1],
+        [8.675418651,-0.242068655,1],
+        [7.673756466,3.508563011,1]]
+
+
+if __name__ == '__main__':
+    # n = nearest_neighbor(get_example(), get_example()[0], 3)
+    # for i in n:
+    #     print(i)
+    for example in get_example():
+        pred = classify(get_example(), example, 3)
+        print("pred is {}".format(pred))
--- utilities.py
+++ utilities.py
@@ -1,14 +1,46 @@
 import numpy as np
+from csv import reader
+
+
+def str_to_float(data, index):
+    for row in data:
+        row[index] = float(row[index].strip())
+
+
+def str_to_int(data, index):
+    vals = [row[index] for row in data]
+    cl = {}
+    for i, v in enumerate(vals):
+        cl[v] = i
+    for row in data:
+        row[index] = cl[row[index]]
+    return cl
+
+
+def load_csv(file_path):
+    og_file = open(file_path, 'r')
+    data_set = og_file.read()
+    og_file.close()
+    rows = data_set.split('\n')
+    data = []
+    for row in rows:
+        row = row.split(',')
+        sub_data = []
+        for sd in row:
+            sub_data.append(sd)
+        data.append(sub_data)
+    for i in range(len(data[0])-1):
+        str_to_float(data, len(data)-1)
+    lk = str_to_int(data, len(data)-1)
+    print("data now looks like {} and dictionary is {}".format(data[0], lk))
+    return data
 
 
 def parse_csv(file_path):
-    data_set = open(file_path, 'r')
-    data = data_set.read()
-    data_set.close()
-    rows = data.split('\n')
-    data_arr = np.array([[c for c in r.split(',')]for r in rows])
-    classes = data_arr[:,-1]
-    features = data_arr[:,:-1]
+    data = load_csv(file_path)
+    classes = [row[:-1] for row in data]
+    features = [row[-1] for row in data]
+    # print('features are {} and classes are {}'.format(features, classes))
     return features, classes
 
 
