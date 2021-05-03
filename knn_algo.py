1ff2d17c5973fedf9b2da4843ef1ec07c8207cce
Emmanuel <eia1803@hsutx.edu>
2021-05-02 19:09:43 -0500

    Added comments

--- knn_algo.py
+++ knn_algo.py
@@ -1,8 +1,18 @@
 import math
-import numpy as np
+
 
 class OutcomeMetrics:
+    """
+    This class determines the recall, precision, as well as for accuracy for
+    the predicted labels in comparison with the actual labels.
+    """
+
     def __init__(self, classifier_labels, actual_labels):
+        """
+        Initialize the class when the object is called.
+        :param classifier_labels: The predicted labels of the algorithm.
+        :param actual_labels: The expected labels that were pre determined.
+        """
         self.classifier_labels = classifier_labels
         self.actual_labels = actual_labels
         self.confusion_matrix = self.__build_confusion_matrix()
@@ -55,6 +65,13 @@ class OutcomeMetrics:
 
 
 def euclidean(all_x, all_y):
+    """
+    Determines the distance between the two single points of two separate
+    lists that are in a euclidean space.
+    :param all_x: A list of values for one point
+    :param all_y: A list of values for another point
+    :return: The square root of the sum of all the points.
+    """
     all_points = zip(all_x, all_y)
     set_diff = []
     for x, y in all_points:
@@ -63,19 +80,36 @@ def euclidean(all_x, all_y):
 
 
 def nearest_neighbor(train_features, train_classes, test_features, depth):
-        dist_list = []
-        fc_test = zip(train_classes, train_features)
-        for cl, row in fc_test:
-            dist = euclidean(row, test_features)
-            dist_list.append((dist, test_features, cl))
-        dist_list.sort(key=lambda d: d[0])
-        neighbor = []
-        for i in range(depth):
-            neighbor.append(dist_list[i][2])#, dist_list[i][2]))
-        return neighbor
+    """
+    Determines the nearest neighbors based off of two points euclidean distance.
+    :param train_features: All of the feature lists used to train the algorithm.
+    :param train_classes: All of the classifications that are in parallel with every list of features.
+    :param test_features: A single list of features used to test the algorithm.
+    :param depth: How many neighbors should be returned.
+    :return: A list of neighbors.
+    """
+    dist_list = []
+    fc_test = zip(train_classes, train_features)
+    for cl, row in fc_test:
+        dist = euclidean(row, test_features)
+        dist_list.append((dist, test_features, cl))
+    dist_list.sort(key=lambda d: d[0])
+    neighbor = []
+    for i in range(depth):
+        neighbor.append(dist_list[i][2])#, dist_list[i][2]))
+    return neighbor
 
 
 def classify(train_features, train_classes, test_features, depth=30):
+    """
+    Predicts what should be the best classification for a test based on the mode of classes received from
+    the neighbors.
+    :param train_features: All of the feature lists used to train the algorithm.
+    :param train_classes: All of the classifications that are in parallel with every list of features.
+    :param test_features: A single list of features used to test the algorithm.
+    :param depth: How many neighbors should be returned.
+    :return: The predicted value.
+    """
     neighbors = nearest_neighbor(train_features, train_classes, test_features, depth)
     class_vals = [cl for cl in neighbors]
     pred = max(class_vals, key=class_vals.count)
